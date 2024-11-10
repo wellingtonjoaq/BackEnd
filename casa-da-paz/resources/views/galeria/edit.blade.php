d490fb8f92928183d0a0a094493a82dec86da901
Thiago da Silva Felipe <tiagofelipe056@gmail.com>
2024-11-10 15:05:27 -0300

    Desenvolvimento galeria

--- casa-da-paz/resources/views/galeria/edit.blade.php
+++ casa-da-paz/resources/views/galeria/edit.blade.php
@@ -2,12 +2,12 @@
 @section('title', 'nova Imagem')
 @section('content')
     <h1>Nova imagem</h1>
-    <form action="{{ route('memorial.update', $memorial) }}" method="POST" enctype="multipart/form-data">
+    <form action="{{ route('galeria.update', $galeria) }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
         <div class="mb-3">
             <label for="nome" class="form-label">Nome</label>
-            <input value="{{ $memorial->nome }}" type="text" class="form-control" id="nome" name="nome" placeholder="Nome da imagem">
+            <input value="{{ $galeria->nome }}" type="text" class="form-control" id="nome" name="nome" placeholder="Nome da imagem">
         </div>
 
         <div class="mb-3">
@@ -15,10 +15,10 @@
             <input type="file" class="form-control" id="imagem" name="imagem" accept="image/*">
         </div>
 
-        @if($memorial->imagem)
+        @if($galeria->imagem)
             <div class="mb-3">
                 <img
-                    src="{{ asset('storage/' . $memorial->imagem) }}"
+                    src="{{ asset('storage/' . $galeria->imagem) }}"
                     alt="Imagem memorial"
                     class="img-thumbnail" width="150"
                 >
@@ -26,8 +26,8 @@
         @endif
 
         <div class="mb-3">
-            <label for="informacao" class="form-label">informacao Imagem</label>
-            <input value="{{ $memorial->informacao }}" type="text" class="form-control" id="informacao" name="informacao" placeholder="Infomação da Imagem">
+            <label for="data" class="form-label">Data do Evento</label>
+            <input value="{{ $galeria->data }}" type="text" class="form-control" id="data" name="data">
         </div>
 
         <button class="btn btn-success" type="submit">Enviar</button>
