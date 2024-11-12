90e69c45f5b81eaed474e6cc15b89b5db661988b
Wellington <wellingtonjoaquim736@gmail.com>
2024-11-12 19:42:12 -0300

    api

--- casa-da-paz/routes/api.php
+++ casa-da-paz/routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\VoluntarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('voluntarios')->group(function () {
+    Route::get('/', [VoluntarioController::class, 'index'])->name('voluntarios.index');
+    Route::get('create', [VoluntarioController::class, 'create'])->name('voluntarios.create');
+    Route::get('{id}', [VoluntarioController::class, 'show'])->name('voluntarios.show');
+    Route::get('{id}/edit', [VoluntarioController::class, 'edit'])->name('voluntarios.edit');
+    Route::post('/', [VoluntarioController::class, 'store'])->name('voluntarios.store');
+    Route::put('{id}', [VoluntarioController::class, 'update'])->name('voluntarios.update');
+    Route::delete('{id}', [VoluntarioController::class, 'destroy'])->name('voluntarios.destroy');
+});
